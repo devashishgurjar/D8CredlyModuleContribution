60f770e30ab032d45ca7220e6452dfa14435b771
devashishgurjar <gurjar_devashish@yahoo.com>
2018-06-15 19:13:45 +0530

    changes of 15 jun 2018

--- modules/custom/credly/src/Form/CredlyAdminSettingForm.php
+++ modules/custom/credly/src/Form/CredlyAdminSettingForm.php
@@ -22,10 +22,17 @@ class CredlyAdminSettingForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $SavedAdminUsername = \Drupal::config('system.site')->get('siteadmincredlyusername');
+    $SavedAdminPassword = \Drupal::config('system.site')->get('siteadmincredlypassword');
+    $SavedAppApiKey = \Drupal::config('system.site')->get('siteadmincredlyapikey');
+    $SavedAppApiSecret = \Drupal::config('system.site')->get('siteadmincredlysecret');
+
     $form['credly_admin_username'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Credly Admin Username'),
       '#description' => $this->t('Enter username for credly account from which App is created'),
+      '#default_value' => $SavedAdminUsername,
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '0',
@@ -34,6 +41,7 @@ class CredlyAdminSettingForm extends FormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Credly Admin Password'),
       '#description' => $this->t('Enter password for credly account from which app is created in credly'),
+      '#default_value' => $SavedAdminPassword,
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '0',
@@ -42,6 +50,7 @@ class CredlyAdminSettingForm extends FormBase {
       '#type' => 'textfield',
       '#title' => $this->t('Credly App API Key'),
       '#description' => $this->t('Open this URl https://developers.credly.com/my-apps copy API key and enter here'),
+      '#default_value' => $SavedAppApiKey,
       '#maxlength' => 64,
       '#size' => 64,
       '#weight' => '0',
@@ -50,6 +59,7 @@ class CredlyAdminSettingForm extends FormBase {
       '#type' => 'textarea',
       '#title' => $this->t('Credly API Secret'),
       '#description' => $this->t('Open the URL https://developers.credly.com/my-app copy the Seceret Key and enter here'),
+      '#default_value' => $SavedAppApiSecret,
       '#weight' => '0',
     ];
     $form['submit'] = [
@@ -72,10 +82,16 @@ class CredlyAdminSettingForm extends FormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     // Display result.
-    foreach ($form_state->getValues() as $key => $value) {
-      drupal_set_message($key . ': ' . $value);
-    }
-
+    $AdminUsername = $form_state->getValue('credly_admin_username');
+    $AdminPassword = $form_state->getValue('credly_admin_password');
+    $AppApiKey = $form_state->getValue('credly_app_api_key');
+    $AppApiSecret = $form_state->getValue('credly_api_secret');
+    $inject_config = \Drupal::service('config.factory')->getEditable('system.site');
+    // Setting and saving site api key value if its something different from default value.
+    $inject_config->set('siteadmincredlyusername', $AdminUsername)->save();
+    $inject_config->set('siteadmincredlypassword', $AdminPassword)->save();
+    $inject_config->set('siteadmincredlyapikey', $AppApiKey)->save();
+    $inject_config->set('siteadmincredlysecret', $AppApiSecret)->save();
   }
 
 }
